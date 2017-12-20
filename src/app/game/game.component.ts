11d3cff4353620c1ba9dfdbe552c0da661657838
Thomas 'SubSide' van den Bulk <Thomasvdbulk@gmail.com>
2017-12-20 10:49:58 +0100

    Added: A way to calculate fibonacci indices
    getFibIndex will return the fibonacci index if it is a fibonacci number,
    or null otherwise. We cache the numbers to reduce calculation time.

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -6,12 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-
-  public playField: number[][];
-
-  constructor() { }
+  // We cache the fibonacci numbers because calculating this 2500 times
+  // because calculating this ~ 99 * 9 times every time you click could be an issue.
+  public fibCache: number[] = [];
+  public checkingNumber = 0;
 
   ngOnInit() {
   }
 
+  getFibIndex(num: number): number {
+    // If we've already cached the number, we'll just return that
+    if (typeof this.fibCache[num] !== 'undefined') {
+      return this.fibCache[num];
+    }
+
+    // We first check if our number is a fibonacci number
+    // This is true if and only if 5x²+4 or 5x²-4 is a perfect square
+    const check1: number = 5 * num * num - 4;
+    const check2: number = check1 + 8;
+
+    if(this.isPerfectSquare(check1) || this.isPerfectSquare(check2)){
+      this.fibCache[num] = this.calculateFibonacciIndex(num);
+    } else {
+      // Else we define it and set it as NULL
+      this.fibCache[num] = null;
+    }
+
+    return this.fibCache[num];
+  }
+
+  private isPerfectSquare(num: number): boolean {
+    return Math.sqrt(num) % 1 === 0;
+  }
+
+  private calculateFibonacciIndex(num: number): number {
+    // We can calculate the index for any number > 1
+    // by taking the phi log of (num * sqrt(5) + 1/2) and then flooring it
+    // At least, that's what Wikipedia says. And it seems to work!
+    return Math.floor(
+      Math.log(num * Math.sqrt(5) + 0.5) /
+      Math.log((1 + Math.sqrt(5)) / 2)
+      );
+  }
 }
